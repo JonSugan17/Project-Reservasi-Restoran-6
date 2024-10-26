b99596e5b81467733a86888f6056d0b3573f2f6a
Axolotl <undertaleforjuandika@gmail.com>
2024-10-26 22:46:45 +0700

    Update Test.pas

--- Test.pas
+++ Test.pas
@@ -1,12 +1,10 @@
-Program Test;
+program Test;
 uses crt;
-
 var
-i :string;
-nama : string;
+  i : string;
+  nama : string;
 
-
-Begin
+begin
     clrscr;
     writeln('Selamat Datang di Restaurant');
     writeln('Hello World');
@@ -15,8 +13,6 @@ Begin
     writeln('moga wangy');
     writeln('dwislamy');
     writeln('Mau tidur dulu');
-    writeln('Bruh');
     write('iwak');
     readln(i);
-
 end.
