4b321697d74e9dd207fdb7e9bb1449c50d2e30f4
Jonathan Sugan <fterdoang873@gmail.com>
2024-11-05 11:29:27 +0700

    Create ProgramUtama.pas

---
+++ ProgramUtama.pas
@@ -0,0 +1,14 @@
+program SistemReservasiRestoran;
+uses crt;
+
+Begin
+    clrscr;
+    writeln('Menu Sistem Reservasi Restoran');
+    writeln('1. Tampilakan Ketersediaan');
+    writeln('2. Reservasi Meja');
+    writeln('3. simpan Data Reservasi');
+    writeln('4. Cari Reservasi');
+    writeln('5. Keluar');
+    writeln('Pilih Menu: '); readln();
+
+end.
