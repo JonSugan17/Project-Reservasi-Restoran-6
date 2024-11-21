15f3e326d908dcae2dababb4dffa5eb934dfeeab
Jonathan Sugan <fterdoang873@gmail.com>
2024-11-21 20:59:58 +0700

    sedikit perbaikan

--- ProgramUtama.pas
+++ ProgramUtama.pas
@@ -94,17 +94,15 @@ begin
     writeln('Reservasi Berhasil!');
 end;
 
-procedure CariReservasi();
+
+// buat pencarian memakai rekursif
+function CariReservasi(var Data: array of Reservasi; Nama: String; Indeks: Integer): Integer;
 begin
-    writeln('wlee');
-    // buat pencarian memakai rekursif
-    function CariReservasi(var Data: array of Reservasi; Nama: String; Indeks: Integer): Integer;
-begin
-  if Indeks > High(Data) then
-    CariReservasi := -1
-  else if Data[Indeks].NamaPemesan = Nama then
-    CariReservasi := Indeks
-  else
+    if Indeks > High(Data) then
+        CariReservasi := -1
+    else if Data[Indeks].NamaPemesan = Nama then
+        CariReservasi := Indeks
+    else
     CariReservasi := CariReservasi(Data, Nama, Indeks + 1);
 end;
 
